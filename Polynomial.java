657f67fe2b4b755f6fb54a9421bd9121b330c8bc
iwanski12 <54420497+iwanski12@users.noreply.github.com>
2019-09-04 15:05:54 -0700

    Add files via upload

---
+++ Polynomial.java
@@ -0,0 +1,21 @@
+
+/**
+ * Skeleton program for Week 4 Lab on Operators and Conditional Branching.
+ *
+ * @author Roy Chancellor
+ * @version 2/6/2019
+ */
+public class Polynomial
+{
+    public static void main( String[] args ) {
+        //variable declarations
+        double a = -1;
+        double b = 2*a*a-7*a+3;
+        
+        //Polynomial computation
+        
+        
+        //Output the result
+        System.out.println("My answer is p(a)" +b);
+    }
+}
