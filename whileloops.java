571172fbfc49c781fc5b9f07ffb1e65abe591ebc
iwanski12 <54420497+iwanski12@users.noreply.github.com>
2019-09-25 15:14:55 -0700

    Create whileloops.java

---
+++ whileloops.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+public class SecretNumber{
+    int secret = 9, guess = secret - 1;
+    
+public class While_loop
+{
+    // instance variables - replace the example below with your own
+    public static void main ( String args[]) {
+     int i, n, num;
+
+    /**
+     * Constructor for objects of class While_loop
+     */
+     
+    {
+        // initialise instance variables
+        i = 0;
+        while (i<=4) {
+            System.out.println(i * i);
+            i++;
+    }
+    System.out.println("The loop finished because i <=4 is false");
+
+
+    }
+     num=10; i=1;
+    while (i <= num/2) {
+        if (num % i==0)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        i++;
+   
+}
+} 
+}
+}
+// I accidentally made 2 classes, the bove code does not work but the following code does
+import java.util.Scanner;
+public class whileLoops
+{
+ public static void main( String args[] ) {
+    int i;
+    double j,k,l;
+    System.out.println("Choose an Option");
+    Scanner keys=new Scanner(System.in);
+    System.out.println("1) Square a Number");
+    System.out.println("2) Cube a Number");
+    System.out.println("3) Quit");
+    
+    i=keys.nextInt();
+    while(i!=3){
+    if(i==1) {
+      
+  
+        System.out.println("Enter a Number to Square");
+         j=keys.nextDouble();
+        k=j*j;
+        System.out.println(k);
+    System.out.println("1) Square a Number");
+    System.out.println("2) Cube a Number");
+    System.out.println("3) Quit");
+    i=keys.nextInt();
+}
+else if(i==2){
+    
+    System.out.println("Enter a Number to Cube");
+     j=keys.nextDouble();
+    
+    l=j*j*j;
+    System.out.println(l);
+    System.out.println("1) Square a Number");
+    System.out.println("2) Cube a Number");
+    System.out.println("3) Quit");
+    i=keys.nextInt();
+} 
+
+
+
+}
+    
+  System.out.println("The program has been closed");  
+
+        
+    
+    
+    
+
+            
+        
+        
+}
+}
